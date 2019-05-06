af9ebf81daaf72c8a61be1dd505b0ce59395d8b3
jinsanity07git <443605310@qq.com>
2019-05-06 17:14:35 -0500

    # OD trip chain

--- Prowl_tripchain2.py
+++ Prowl_tripchain2.py
@@ -10,32 +10,40 @@ len(df_raw)   # 198279
 """
 Index(['Date', 'Time', 'Stop', 'OnOffCam'], dtype='object')
 """
-
 Stud_ID = df_raw.index.unique()
 len(Stud_ID)  #7572   3085
 
 stud = 8
-df_person = df_raw[df_raw.index == Stud_ID[stud] ]
 
-df_ser = df_person.groupby(['OnOffCam','Stop'])['Stop'].size()
+def student_max(stud = 1, df_raw= df_raw ):
+    df_person = df_raw[df_raw.index == Stud_ID[stud] ]
+    df_ser = df_person.groupby(['OnOffCam','Stop'])['Stop'].size()
+    max_0,max_1 = 0,0
+    for i,n in enumerate(df_ser):
+        index_tup = df_ser.index[i]
+        # print (index_tup[1] ,n) 
 
-
-df_ser[0,'Non-Peak Hours UWM KIRC (KIRC-S)']
-type(df_ser[0,])
-
-
-for i,n in enumerate(df_ser):
-    index_tup = df_ser.index[i]
-    print (index_tup[1] ,n) 
-    max_0 = 0
-    max_1 = 0
-    if 0 in index_tup:
-        if df_ser[index_tup] > max_0:
-            max_0 = df_ser[index_tup]
-            inx_stop0 =  index_tup[1]
-    else:
-        if df_ser[index_tup] > max_1:
-            max_1 = df_ser[index_tup]
-            inx_stop1 =  index_tup[1]
-
+        if 0 in index_tup:
+            if df_ser[index_tup] > max_0:
+                max_0 = df_ser[index_tup]
+                inx_stop0 =  index_tup[1]
+        else:
+            if df_ser[index_tup] > max_1:
+                max_1 = df_ser[index_tup]
+                inx_stop1 =  index_tup[1]
+    return  (Stud_ID[stud], inx_stop0,max_0,inx_stop1,max_1)
     # print (i,n)
+
+
+student_max(stud = 2322)[2]
+
+# tup = student_max(stud = i)
+
+# '{},{},{},{},{}'.format(100968,'UWM Golda Meir Library (Lib)',29,'RiverView Residence Hall (RVW)',32)
+
+outfile = 'Data/tripchain_df1.csv'
+with open(outfile,'w') as file:
+    file.write('ID,Stop_0,Cnt_0,Stop_1,Cnt_1 \n')
+    for i in range(0,len(Stud_ID)-1):
+        tup = student_max(stud = i)
+        file.write('{},{},{},{},{} \n'.format(tup[0],tup[1],tup[2],tup[3],tup[4]))
---
+++ Prowl_tripchain3.py
@@ -0,0 +1,49 @@
+
+
+import pandas as pd
+fname= 'tripchain_df1.csv'
+path = 'Data/'
+df_raw = pd.read_csv(path+ fname,index_col='ID')
+df_raw.head(10)
+df_raw.columns
+len(df_raw)   # 3084
+"""
+Index(['Stop_0', 'Cnt_0', 'Stop_1', 'Cnt_1 '], dtype='object')
+"""
+
+on_campus = ['Cunningham Hall (Cunn)',
+            'UWM Golda Meir Library (Lib)' ,
+            'Sandburg Residence Hall (Sand)' ,
+            'UWM Union (Union)',
+            'Non-Peak Hours UWM KIRC (KIRC-S)'
+            ]
+off_campus =['Cambridge Commons (CC-S)',
+            'Cambridge Commons - Northbound (CC-N)',
+            'Capitol and Humboldt Park and Ride (C/H)',
+            'Kenilworth Square Apartments (KNW)',
+            'RiverView Residence Hall (RVW)'
+            ]
+
+df_raw[df_raw['Stop_0']==on_campus[0]  ]
+
+df_sub = df_raw.loc[ (df_raw['Stop_0']==on_campus[3]) & (df_raw['Stop_1']==off_campus[3] )  ]
+
+
+sum(df_sub['Cnt_0'])
+sum(df_sub['Cnt_1 '])
+
+
+
+
+outfile = 'Data/tripchain_df2.csv'
+with open(outfile,'w') as file:
+    file.write('Stop_0,Stop_1,Cnt_0,Cnt_1,Ave\n')
+    for i in range(0,len(on_campus)):
+        for j in range(0,len(off_campus)):
+            Stop_0,Stop_1= on_campus[i],off_campus[j]
+            df_sub = df_raw.loc[ (df_raw['Stop_0']== Stop_0) & (df_raw['Stop_1']==Stop_1)]
+            Cnt_0 = sum(df_sub['Cnt_0'])
+            Cnt_1 = sum(df_sub['Cnt_1 '])
+            Ave = (Cnt_0+ Cnt_1)*0.5
+
+            file.write('{},{},{},{},{} \n'.format(Stop_0,Stop_1,Cnt_0,Cnt_1,Ave))
