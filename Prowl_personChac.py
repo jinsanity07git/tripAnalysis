a64d9587cedc985c684e9296b8a4a8e5c0abcf3b
jinsanity07git <31701385+jinsanity07git@users.noreply.github.com>
2020-01-06 15:42:39 -0600

    git setting

--- Prowl_personChac.py
+++ Prowl_personChac.py
@@ -15,7 +15,7 @@ Index(['Date', 'Time', 'Stop', 'OnOffCam'], dtype='object')
 Stud_ID = df_raw.index.unique()
 len(Stud_ID)  #7572
 
-stud = 1210
+stud = 1280
 df_person = df_raw[df_raw.index == Stud_ID[stud] ]
 # df_person.groupby(['OnOffCam','Stop','Date']).count()
 
@@ -29,4 +29,18 @@ df_person.groupby(['OnOffCam'])['Date'].count()
 # OD Infering---- On-off Campus
 df_person.groupby(['OnOffCam','Stop'])['Date'].count()
 
+
+df_person.count()[1]
+
+###01 analyze students' count profile
+travel_count = []
+for stud in range(0,len(Stud_ID)):
+    # print(stud)
+    df_person = df_raw[df_raw.index == Stud_ID[stud] ]
+    num = df_person.count()[1]
+    travel_count.append(num)
+
+s = pd.Series(travel_count)
+print ( s.describe() )
+s.hist( bins=40)
 
